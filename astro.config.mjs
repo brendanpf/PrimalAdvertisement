25636cbc0fc92cdfd8d7ed2f11c75f2f925e22bf
brendanpf <brendanpfrayne@gmail.com>
2024-10-02 17:33:10 +0100

    start over

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
-  base: 'my-repo',
   integrations: [
     tailwind(),
     react() 
