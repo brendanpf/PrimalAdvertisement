02c55e17c0f4b209a6bf4860a43d2513febc46cc
brendanpf <brendanpfrayne@gmail.com>
2024-10-16 21:53:05 +0100

    romoval of base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'http://www.primaladvertisement.com/',
-  base: '/PrimalAdvertisement/',
   integrations: [
     tailwind(),
     react() 
