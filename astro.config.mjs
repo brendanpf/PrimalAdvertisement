7c4b00179459fa4d27c0bf917b6273f646db1383
brendanpf <brendanpfrayne@gmail.com>
2024-10-16 21:14:48 +0100

    site change 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'primaladvertisement.com/',
+  site: 'http://primaladvertisement.com/',
   base: '/PrimalAdvertisement/',
   integrations: [
     tailwind(),
