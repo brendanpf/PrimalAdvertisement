706a76a9d22f9041ea185d95d3fd095e03bb8bf5
brendanpf <brendanpfrayne@gmail.com>
2024-10-16 20:19:51 +0100

    config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://brendanpf.github.io//',
-  base: '//',
+  site: 'https://brendanpf.github.io/PrimalAdvertisement/',
+  base: '/PrimalAdvertisement/',
   integrations: [
     tailwind(),
     react() 
