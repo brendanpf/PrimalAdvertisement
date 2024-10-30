992d4fac0d9aa5fd8c5807b7f20f2f6141338e17
brendanpf <brendanpfrayne@gmail.com>
2024-10-30 22:43:34 +0000

    added paypal API

--- src/components/PayPalButton.jsx
+++ src/components/PayPalButton.jsx
@@ -1,47 +1,46 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import { loadScript } from '@paypal/paypal-js';
 
-function PayPalButton({ amount }) {
-  const paypalRef = useRef();
+const PayPalButton = ({ price }) => {
+  const [isSdkReady, setIsSdkReady] = useState(false);
 
   useEffect(() => {
-    if (!window.paypal) return;
-
-    window.paypal
-      .Buttons({
-        style: {
-          shape: 'rect',
-          color: 'blue',
-          layout: 'vertical',
-          label: 'paypal',
-        },
-        createOrder: (_data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: amount,
-                  currency_code: 'EUR',
-                },
-              },
-            ],
-          });
-        },
-        onApprove: (_data, actions) => {
-          return actions.order.capture().then((details) => {
-            // Handle successful transaction here
-            alert(`Transaction completed by ${details.payer.name.given_name}!`);
-            // Optionally redirect or update your UI
-          });
-        },
-        onError: (err) => {
-          // Handle errors here
-          console.error(err);
-        },
+    loadScript({ 'client-id': 'AUG953QzU7OIh1NorvtQ1BgQqm4AwUUbdkgNlC_RkqQpCQxxmQVF4se8QQFYu9ATwa0CUpFiKCs7PMZT', 'currency': 'EUR' })
+      .then(() => {
+        setIsSdkReady(true);
       })
-      .render(paypalRef.current);
-  }, [amount]);
+      .catch((err) => {
+        console.error('Failed to load PayPal SDK script', err);
+      });
+  }, []);
 
-  return <div ref={paypalRef} />;
-}
+  useEffect(() => {
+    if (isSdkReady) {
+      paypal.Buttons({
+        createOrder: function (data, actions) {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: price,
+
+              },
+            }],
+          });
+        },
+        onApprove: function (data, actions) {
+          return actions.order.capture().then(function (details) {
+            alert('Transaction completed by ' + details.payer.name.given_name);
+          });
+        },
+      }).render(`#paypal-button-container-${price}`);
+    }
+  }, [isSdkReady, price]);
+
+  return (
+    <div id={`paypal-button-container-${price}`}>
+      {isSdkReady ? null : <p>Loading PayPal...</p>}
+    </div>
+  );
+};
 
 export default PayPalButton;
